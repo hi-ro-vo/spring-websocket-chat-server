135ca941a660e95991c65d0bff0e5004811323bf
sharovdenis <ytrewq123456789000@gmail.com>
2021-05-30 18:05:06 +0300

    authenticatior fix

--- src/main/java/sample/AuthChannelInterceptorAdapter.java
+++ src/main/java/sample/AuthChannelInterceptorAdapter.java
@@ -1,11 +1,13 @@
 package sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -18,20 +20,33 @@ public class AuthChannelInterceptorAdapter implements ChannelInterceptor {
     private final WebSocketAuthenticatorService webSocketAuthenticatorService;
 
     @Autowired
+    @Qualifier("clientOutboundChannel")
+    private MessageChannel clientOutboundChannel;
+
+    @Autowired
     public AuthChannelInterceptorAdapter(final WebSocketAuthenticatorService webSocketAuthenticatorService) {
         this.webSocketAuthenticatorService = webSocketAuthenticatorService;
     }
 
     @Override
-    public Message<?> preSend(final Message<?> message, final MessageChannel channel) throws AuthenticationException {
+    public Message<?> preSend(final Message<?> message, final MessageChannel channel){
         final StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
         if (StompCommand.CONNECT == accessor.getCommand()) {
             final String username = accessor.getFirstNativeHeader(USERNAME_HEADER);
             final String password = accessor.getFirstNativeHeader(PASSWORD_HEADER);
+            final String sessionId = accessor.getSessionId();
 
-                final UsernamePasswordAuthenticationToken user = webSocketAuthenticatorService.getAuthenticatedOrFail(username, password);
-                accessor.setUser(user);
+                try {
+                    final UsernamePasswordAuthenticationToken user = webSocketAuthenticatorService.getAuthenticatedOrFail(username, password);
+                    accessor.setUser(user);
+                } catch (AuthenticationException error){
+                    StompHeaderAccessor headerAccessor = StompHeaderAccessor.create(StompCommand.ERROR);
+                    headerAccessor.setMessage(error.getMessage());
+                    headerAccessor.setSessionId(sessionId);
+                    clientOutboundChannel.send(MessageBuilder.createMessage(new byte[0], headerAccessor.getMessageHeaders()));
+                }
+
         }
         return message;
     }
